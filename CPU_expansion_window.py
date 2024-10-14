3239718e602f49aeffe5d0d9a2ab3f016dd6c1aa
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-14 21:28:36 +0300

    Добавлены геттеры для снятия данных для процессора

--- CPU_expansion_window.py
+++ CPU_expansion_window.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget
 from PyQt6.QtGui import QIcon
 import cpuinfo
+import psutil
 from style import CONST_WINDOW_SYSTEM_MONITORING
 
 
@@ -23,7 +24,6 @@ class ProcessorAdvansed(QMainWindow):
         multithreading_support = QLabel(text="Поддержка многопточности")
         hardware_encryption_support = QLabel(text="Поддержка аппаратного шифрования")
         vector_instructions = QLabel(text="Проверка поддержки векторных инструкций")
-        temp_cpu = QLabel(text="Температура процессора")
         
         control_UI.addWidget(cpu_name)
         control_UI.addWidget(cpu_arch)
@@ -35,7 +35,6 @@ class ProcessorAdvansed(QMainWindow):
         control_UI.addWidget(multithreading_support)
         control_UI.addWidget(hardware_encryption_support)
         control_UI.addWidget(vector_instructions)
-        control_UI.addWidget(temp_cpu)
         
         center_w.setLayout(control_UI)
         
@@ -56,3 +55,50 @@ class GetCPU:
         
     def all_result(self):
         return self.contol_data
+    
+    def get_name_CPU(self):
+        return self.contol_data.get('brand_raw')
+    
+    def get_arch_CPU(self):
+        return self.contol_data.get('arch')
+    
+    def get_hz_actual_CPU(self):
+        return self.contol_data.get('hz_actual_friendly')
+    
+    def get_hz_advertised_CPU(self):
+        return self.contol_data.get('hz_advertised_friendly')
+    
+    def get_L2_cache_CPU(self):
+        return str(int(self.contol_data.get('l2_cache_size')) / (1024 * 1024))
+    
+    def get_L3_cache_CPU(self):
+        return str(int(self.contol_data.get('l3_cache_size')) / (1024 * 1024))
+    
+    def get_bool_ht_CPU(self):
+        check = self.contol_data.get('flags')
+        
+        if "ht" in check:
+            return "Поддержка многопоточности присутствует"
+        else:
+            return "Нет поддержки многопоточности"
+    
+    def get_bool_aes_CPU(self):
+        check = self.contol_data.get('flags')
+        
+        if "aes" in check:
+            return "Поддержка аппаратного шифрования присутствует"
+        else:
+            return "Нет поддержки аппаратного шифрования"
+        
+    def get_bool_vec_CPU(self):
+        check = self.contol_data.get('flags')
+        
+        if (("avx" in check) and ("avx2" in check) and ("sse" in check)
+            and ("sse2" in check) and ("sse4_1" in check) and ("sse4_2" in check)):
+            
+            return "Поддержка векторных инструкций присутствует"
+        else:
+            return "Нет поддержки векторных инструкций"
+        
+    def get_count_CPU(self):
+        return self.contol_data.get('count')

b8f9116efab525c5ffb24877f2878c27d8038245
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-14 20:49:53 +0300

    Добавлено получение значений, далее установка их в окно

--- GPU_expansion_window.py
+++ GPU_expansion_window.py
@@ -39,17 +39,54 @@ class GetGPU:
     def __init__(self) -> None:
         self.control_data = GPUtil.getGPUs()
     
-    def all_GPU(self):    
-        result = []
+    def get_name_GPU(self):
+        call = []
+        result = ''
     
         for i in range(len(self.control_data)):
-            gpu_data = self.control_data[i]
+            get_gpu_data = self.control_data[i]
         
-            result.append({
-            'GPU_name': getattr(gpu_data, 'name', 'Unknown'), 
-            'temperature': getattr(gpu_data, 'temperature', 'Unknown'), 
-            'memory_used': getattr(gpu_data, 'memory_used', 'Unknown'),  
-            'memory_total': getattr(gpu_data, 'memory_total', 'Unknown')  
-            })
+            call.append({'GPU_name': getattr(get_gpu_data, 'name', 'Unknown'),})
+            result += call[i].get('GPU_name')
+        
+    
+        return result
+    
+    def get_temp_GPU(self):
+        call = []
+        result = ''
+    
+        for i in range(len(self.control_data)):
+            get_gpu_data = self.control_data[i]
+        
+            call.append({'temperature': getattr(get_gpu_data, 'temperature', 'Unknown'),})
+            result += str(call[i].get('temperature'))
+        
+    
+        return result
+    
+    def get_memory_used_GPU(self):
+        call = []
+        result = ''
+    
+        for i in range(len(self.control_data)):
+            get_gpu_data = self.control_data[i]
+        
+            call.append({'memoryUsed': getattr(get_gpu_data, 'memoryUsed', 'Unknown'),})
+            result += str(call[i].get('memoryUsed'))
+        
+    
+        return result
+    
+    def get_memory_total_GPU(self):
+        call = []
+        result = ''
+    
+        for i in range(len(self.control_data)):
+            get_gpu_data = self.control_data[i]
+        
+            call.append({'memoryTotal': getattr(get_gpu_data, 'memoryTotal', 'Unknown'),})
+            result += str(call[i].get('memoryTotal'))
+        
     
         return result

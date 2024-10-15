63b0fffaebf5852e54024e359b4e8544a62d3318
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-15 20:18:49 +0300

    Добавлены графики с эксперементальными значениями

--- graph_window.py
+++ graph_window.py
@@ -1,28 +1,36 @@
-from PyQt6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QSizePolicy
 from PyQt6.QtGui import QIcon
 from style import CONST_WINDOW_SYSTEM_MONITORING
+from draw_graph import DrawGraph
 
 class GrapgIndicators(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setFixedSize(800, 600)
-        
+
     def draw_window(self):
         contol_UI = QVBoxLayout()
         centr_w = QWidget()
+
+        graph = DrawGraph()
         
-        graph_cpu = QLabel(text="График процессора(Нагрузка): ")
-        graph_gpu = QLabel(text="График графического процессора(Нагрузка): ")
-        graph_temp_gpu = QLabel(text="График графического процессора(температура): ")
-        
-        contol_UI.addWidget(graph_cpu)
-        contol_UI.addWidget(graph_gpu)
-        contol_UI.addWidget(graph_temp_gpu)
-        
+        graph.draw_graph_cpu([1, 2, 3, 4])
+        graph.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)  # Политика размера
+        contol_UI.addWidget(graph)
+
+        graph = DrawGraph()
+        graph.draw_graph_gpu([1, 2, 3, 4])
+        graph.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        contol_UI.addWidget(graph)
+
+        graph = DrawGraph()
+        graph.draw_graph_gpu_temp([1, 2, 3, 4])
+        graph.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        contol_UI.addWidget(graph)
+
         centr_w.setLayout(contol_UI)
-        
         self.setCentralWidget(centr_w)
-    
+
     def data_inst(self):
         icon = "icon.png"
         app_ico = QIcon(icon)
